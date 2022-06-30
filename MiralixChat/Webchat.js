56adee870350a50694eaaefab33ef4db5d685635
frankfj <ffj@dsr.dk>
2022-06-30 13:44:59 +0200

    Add files via upload

--- MiralixChat/Webchat.js
+++ MiralixChat/Webchat.js
@@ -84,7 +84,7 @@ async function renderWebChat() {
 
         botAvatarBackgroundColor: '#1D202D',
         botAvatarBorderColor: '#F4F4F4',
-        botAvatarImage: 'INDSÆT STI TIL VALGFRIT BILLEDE DER REPRÆSENTERE AGENTEN I SAMTALEN',
+        botAvatarImage: 'kermit.png',
         botAvatarInitials: '',
         bubbleBackground: '#126586a1',
         bubbleBorderColor: '#F4F4F4',
